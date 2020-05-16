b3c1418a44be98ab88c7efc031006cafb5a0f8f1
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-15 22:50:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CriancaEducada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase");
+            string educacao = Console.ReadLine();           
+             Console.WriteLine(educacao .Replace("chato", "#@$%*!&") .Replace("chata", "#@$%*!&") .Replace("bobo", "#@$%*!&") .Replace("boba", "#@$%*!&") .Replace("feio", "#@$%*!&") .Replace("feia", "#@$%*!&") .Replace("boboca", "#@$%*!&") .Replace("bocó", "#@$%*!&") .Replace("tonto", "#@$%*!&") .Replace("tonta", "#@$%*!&") .Replace("palerma", "#@$%*!&"). Replace ("paspalha", "#@$%*!&") .Replace("tantã", "#@$%*!&") .Replace("paneca", "#@$%*!&") .Replace("pentelho", "#@$%*!&") .Replace("pentelha", "#@$%*!&") .Replace("burro", "#@$%*!&") .Replace("burra", "#@$%*!&"));
+             Console.ReadLine();          
+        }
+    }
+    
+}
+
